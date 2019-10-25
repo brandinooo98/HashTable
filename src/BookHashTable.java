3fa97e103b9f44d989f68dd7312f5e7a2e0bb96f
Brandinooo98 <brandinooo98@gmail.com>
2019-10-25 18:00:53 -0500

    Debugging

--- src/BookHashTable.java
+++ src/BookHashTable.java
@@ -1,173 +1,188 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
-
-// TODO: comment and complete your HashTableADT implementation
-//
-// TODO: implement all required methods
-// DO ADD REQUIRED PUBLIC METHODS TO IMPLEMENT interfaces
-//
-// DO NOT ADD ADDITIONAL PUBLIC MEMBERS TO YOUR CLASS 
-// (no public or package methods that are not in implemented interfaces)
-//
-// TODO: describe the collision resolution scheme you have chosen
-// identify your scheme as open addressing or bucket
-//
-// if open addressing: describe probe sequence 
-// if buckets: describe data structure for each bucket
-//
-// TODO: explain your hashing algorithm here 
-// NOTE: you are not required to design your own algorithm for hashing,
-//       since you do not know the type for K,
-//       you must use the hashCode provided by the <K key> object
-
-/** HashTable implementation that uses:
+/**
+ * This HashTable implementation uses chained buckets of an array list that holds linked lists
+ * The hashing algorithm used for the HashTable uses the Java hashCode() method, key.hashCode() % TableSize
+ * HashTable implementation that uses:
+ *
  * @param <K> unique comparable identifier for each <K,V> pair, may not be null
  * @param <V> associated value with a key, value may be null
+ * @author Brandon Erickson
  */
 public class BookHashTable implements HashTableADT<String, Book> {
 
-    /** The initial capacity that is used if none is specifed user */
+    /**
+     * The initial capacity that is used if none is specifed user
+     */
     static final int DEFAULT_CAPACITY = 101;
-    
-    /** The load factor that is used if none is specified by user */
+
+    /**
+     * The load factor that is used if none is specified by user
+     */
     static final double DEFAULT_LOAD_FACTOR_THRESHOLD = 0.75;
-    private double loadFactorThreshold;
-    private ArrayList<LinkedList<BookNode>> hashTable;
-    private int capacity;
-    private int numKeys;
+    private double loadFactorThreshold; // Load factor threshold used throughout implementation
+    private ArrayList<LinkedList<BookNode>> hashTable; // Data structure that stores the hash table
+    private int capacity; // Size of the hash table
+    private int numKeys; // Number of keys within the hash table
 
     /**
      * REQUIRED default no-arg constructor
-     * Uses default capacity and sets load factor threshold 
+     * Uses default capacity and sets load factor threshold
      * for the newly created hash table.
      */
     public BookHashTable() {
-        this(DEFAULT_CAPACITY,DEFAULT_LOAD_FACTOR_THRESHOLD);
+        this(DEFAULT_CAPACITY, DEFAULT_LOAD_FACTOR_THRESHOLD);
     }
-    
+
     /**
-     * Creates an empty hash table with the specified capacity 
+     * Creates an empty hash table with the specified capacity
      * and load factor.
-     * @param initialCapacity number of elements table should hold at start.
+     *
+     * @param initialCapacity     number of elements table should hold at start.
      * @param loadFactorThreshold the ratio of items/capacity that causes table to resize and rehash
      */
     public BookHashTable(int initialCapacity, double loadFactorThreshold) {
-        // TODO: comment and complete a constructor that accepts initial capacity 
-        // and load factor threshold and initializes all fields
         capacity = initialCapacity;
         this.loadFactorThreshold = loadFactorThreshold;
-        hashTable = new ArrayList<>(capacity);
-        for(int i = 0; i < capacity; i++)
+        hashTable = new ArrayList<>(capacity); // Creates the HashTable
+        // Adds a LinkedList bucket to every index of the HashTable
+        for (int i = 0; i < capacity; i++)
             hashTable.add(new LinkedList<>());
     }
 
-    private class BookNode{
-        private Book book;
-        private String key;
+    /**
+     * Nodes that store a key/value pair to be stored in HashTable
+     *
+     */
+    private class BookNode {
+        private Book book; // Value
+        private String key; // Key
 
-        private BookNode(String key, Book book){
+        /**
+         * Initializes book and key variables
+         *
+         * @param key - String key that is used to identify node
+         * @param book - Book stored within the node
+         */
+        private BookNode(String key, Book book) {
             this.book = book;
             this.key = key;
         }
     }
 
-    // Returns the load factor for this hash table
-    // that determines when to increase the capacity
-    // of this hash table
+    /**
+     * Returns the loadFactorThreshold of the HashTable
+     *
+     * @return - LoadFactorThreshold of the HashTable
+     */
     @Override
     public double getLoadFactorThreshold() {
         return loadFactorThreshold;
     }
 
-    // Capacity is the size of the hash table array
-    // This method returns the current capacity.
-    //
-    // The initial capacity must be a positive integer, 1 or greater
-    // and is specified in the constructor.
-    //
-    // REQUIRED: When the load factor is reached,
-    // the capacity must increase to: 2 * capacity + 1
-    //
-    // Once increased, the capacity never decreases
+    /**
+     * Returns the capacity of the HashTable
+     *
+     * @return - Capacity of the HashTable
+     */
     @Override
     public int getCapacity() {
         return hashTable.size();
     }
 
-    // Returns the collision resolution scheme used for this hash table.
-    // Implement this ADT with one of the following collision resolution strategies
-    // and implement this method to return an integer to indicate which strategy.
-    //
-    // 1 OPEN ADDRESSING: linear probe
-    // 2 OPEN ADDRESSING: quadratic probe
-    // 3 OPEN ADDRESSING: double hashing
-    // 4 CHAINED BUCKET: array list of array lists
-    // 5 CHAINED BUCKET: array list of linked lists
-    // 6 CHAINED BUCKET: array list of binary search trees
-    // 7 CHAINED BUCKET: linked list of array lists
-    // 8 CHAINED BUCKET: linked list of linked lists
-    // 9 CHAINED BUCKET: linked list of of binary search trees
+    /**
+     * Returns the collision resolution scheme used for this HashTable
+     *
+     * @return - int corresponding to a collision resolution scheme
+     */
     @Override
     public int getCollisionResolutionScheme() {
         return 5;
     }
 
-    // Add the key,value pair to the data structure and increase the number of keys.
-    // If key is null, throw IllegalNullKeyException;
-    // If key is already in data structure, throw DuplicateKeyException();
+    /**
+     * Adds a given key, value pair to the HashTable
+     *
+     * @param key - Key of node to be inserted
+     * @param value - Value of node to be inserted
+     * @throws IllegalNullKeyException - If given key is null
+     * @throws DuplicateKeyException - If given key is already in the data structure
+     */
     @Override
     public void insert(String key, Book value) throws IllegalNullKeyException, DuplicateKeyException {
-        if(key == null)
+        // If given key is null
+        if (key == null)
             throw new IllegalNullKeyException();
-        if(search(key, hashTable.get(Math.abs(key.hashCode() % capacity))) != 0)
+        // If given key is already in the HashTable
+        if (search(key, hashTable.get(Math.abs(key.hashCode() % capacity))) != 0)
             throw new DuplicateKeyException();
         else {
+            // Adds node to correct bucket using hash function
             hashTable.get(Math.abs(key.hashCode() % capacity)).add(new BookNode(key, value));
             numKeys++;
-            if(numKeys / capacity > loadFactorThreshold)
+            // Checks if HashTable needs to be resized after insert
+            if (numKeys / capacity > loadFactorThreshold)
                 resize();
         }
     }
 
+    /**
+     * Once loadFactorThreshold has been reached, increases the size of the HashTable and rehashes nodes back into the HashTable
+     *
+     * @throws IllegalNullKeyException - If key to be inserted is null
+     * @throws DuplicateKeyException - If node has already been inserted
+     */
     private void resize() throws IllegalNullKeyException, DuplicateKeyException {
-        ArrayList<LinkedList<BookNode>> bookList = new ArrayList<>();
-        for(int i = 0; i < capacity; i++) {
-            if(hashTable.get(i).size() != 0) {
-                bookList.add(hashTable.get(i));
-                hashTable.remove(i);
-                hashTable.add(new LinkedList<>());
+        // Temporary ArrayList used to store nodes already within HashTable
+        ArrayList<BookNode> bookList = new ArrayList<>();
+        // Iterates through HashTable collecting buckets
+        for (int i = 0; i < capacity; i++) {
+            // If bucket isn't empty
+            if (hashTable.get(i).size() != 0) {
+                // Iterates through bucket adding nodes to temporary ArrayList
+                for (int j = 0; j < hashTable.get(i).size(); j++) {
+                    bookList.add(hashTable.get(i).get(j));
+                }
+                hashTable.get(i).clear(); // Clears bucket once all nodes are added to the temporary ArrayList
             }
         }
-        int newCapacity = 2 * capacity + 1;
+
+        int newCapacity = 2 * capacity + 1; // Calculates the new capacity
+        // Adds new buckets to the HashTable
         for (int i = 0; i < newCapacity - capacity; i++) {
             hashTable.add(new LinkedList<>());
         }
+
         capacity = newCapacity;
-        for(int i = 0; i < bookList.size(); i++) {
-            for(int j = 0; j < bookList.get(i).size(); j++) {
-                BookNode bookNode = bookList.get(i).get(j);
-                insert(bookNode.key, bookNode.book);
-                numKeys--;
-            }
+        // Rehashes all nodes back into the HashTable
+        for (int i = 0; i < bookList.size(); i++) {
+            BookNode bookNode = bookList.get(i);
+            insert(bookNode.key, bookNode.book);
+            numKeys--;
         }
     }
 
-    // If key is found,
-    //    remove the key,value pair from the data structure
-    //    decrease number of keys.
-    //    return true
-    // If key is null, throw IllegalNullKeyException
-    // If key is not found, return false
+    /**
+     * Removes a key, value pair from the data structure
+     *
+     * @param key - Key of node to be removed
+     * @return - True if node was removed, false otherwise
+     * @throws IllegalNullKeyException - If given key is null
+     */
     @Override
     public boolean remove(String key) throws IllegalNullKeyException {
-        if(key == null)
+        // If key is null throws an exception
+        if (key == null)
             throw new IllegalNullKeyException();
-        else{
+        else {
+            // Bucket where key to remove is located
             LinkedList<BookNode> bucket = hashTable.get(Math.abs(key.hashCode() % capacity));
-            int bookIndex = search(key, bucket);
-            if(bookIndex == 0)
+            int bookIndex = search(key, bucket); // Index of node to remove within bucket
+            // If key was not found returns false
+            if (bookIndex == 0)
                 return false;
+            // If key was found removes node from LinkedList
             else {
                 bucket.remove(bookIndex - 1);
                 numKeys--;
@@ -176,36 +191,57 @@ public class BookHashTable implements HashTableADT<String, Book> {
         }
     }
 
-    // Returns the value associated with the specified key
-    // Does not remove key or decrease number of keys
-    //
-    // If key is null, throw IllegalNullKeyException
-    // If key is not found, throw KeyNotFoundException().
+    /**
+     * Returns the value associated with the specified key
+     * Does not remove key or decrease number of keys
+     *
+     * @param key - Key of node to get
+     * @return - Value of node to get
+     * @throws IllegalNullKeyException - If key is null
+     * @throws KeyNotFoundException - If key is not found
+     */
     @Override
     public Book get(String key) throws IllegalNullKeyException, KeyNotFoundException {
-        if(key == null)
+        // If key is null throws exception
+        if (key == null)
             throw new IllegalNullKeyException();
         else {
+            // Location within bucket
             int bookIndex = search(key, hashTable.get(Math.abs(key.hashCode() % capacity)));
+            // If key was not found throws exception
             if (bookIndex == 0)
                 throw new KeyNotFoundException();
+            // If key was found returns book
             else
                 return hashTable.get(Math.abs(key.hashCode() % capacity)).get(bookIndex - 1).book;
         }
     }
 
+    /**
+     * Finds a node's index within a bucket
+     *
+     * @param key - Key of node to find
+     * @param list - Bucket to search within
+     * @return - Index of node within bucket
+     */
     private int search(String key, LinkedList<BookNode> list) {
-        int counter = 1;
-        for(BookNode node : list) {
+        int counter = 1; // Counts amount of nodes iterated through
+        // Iterates through LinkedList until key is found or end of LinkedList is reached
+        for (BookNode node : list) {
+            // If key is found
             if (node.key.equals(key))
                 return counter;
             else
                 counter++;
         }
-        return 0;
+        return 0; // If key is not found
     }
 
-    // Returns the number of key,value pairs in the data structure
+    /**
+     * Returns the number of key, value pairs in the data structure
+     *
+     * @return - Number of key, value pairs
+     */
     @Override
     public int numKeys() {
         return numKeys;
